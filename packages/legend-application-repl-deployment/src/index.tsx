402767bc55411d2e0de89ae438d67fff296f0081
Gayathri <rallapalle.gayathrichowdary@ny.email.gs.com>
2024-06-07 15:43:45 +0530

    Add filter panel to DataCube

--- packages/legend-application-repl-deployment/src/index.tsx
+++ packages/legend-application-repl-deployment/src/index.tsx
@@ -16,7 +16,8 @@
 
 import { LegendREPLGridClientWebApplication } from '@finos/legend-application-repl';
 import './index.scss';
+import config from '../repl.config.js';
 
 // Resolve baseUrl relatively for application to work in vscode code-server
 const relativeBaseUrl = new URL('./', window.location.href).pathname;
-LegendREPLGridClientWebApplication.run(relativeBaseUrl);
+LegendREPLGridClientWebApplication.run(config.baseUrl);
